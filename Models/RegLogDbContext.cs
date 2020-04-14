4c964e802beededf8c3ef75acc2fbb9d02e56d3f
Adeboye Omotayo Sylvia <60438004+OmoboyeSylvia@users.noreply.github.com>
2020-04-14 14:50:20 +0100

    Add files via upload

---
+++ Models/RegLogDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegLog.Models
+{
+    public class RegLogDbContext : DbContext
+
+    {
+        public string ConnectionString;
+
+
+        public RegLogDbContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+      
+        public RegLogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+﻿using RegLog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegLog.Data
+{
+    public class User
+    {
+        private RegLogDbContext context;
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string password { get; set; }
+
+        public User(string firstName, string lastName, string email, string phone, string password)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.phone = phone;
+            this.password = password;
+        }
+    }
+}
